000621c09ddf8b1231623d812fcef1d5c23afd6d
Suhyxn <momentomori21489@gmail.com>
2023-01-18 14:56:32 +0900

    feat: Linking Tabs.tsx to DetailPage.tsx

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Tabs from "../components/Tabs";
 
 type Params = {
   id: string;
 };
 
+type Tab = "about" | "stats" | "evolution";
+
 function DetailPage() {
   const { id } = useParams<Params>();
-  console.log(id);
+  const [selectedTab, setSelectedTab] = useState<Tab>("about");
+
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
 
   return (
     <>
-      <div>DetailPage id:{id}</div>
-      <div>{id}</div>
+      <Tabs
+        tab={selectedTab}
+        onClick={handleClick}
+        color={{ name: "red", url: "" }}
+      />
     </>
   );
 }
