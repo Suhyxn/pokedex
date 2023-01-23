05a4fac40494832ffc92273706431414cf786f8d
Suhyxn <momentomori21489@gmail.com>
2023-01-23 17:48:33 +0900

    feat: Add usePokemon.ts evolution related API

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,15 +1,31 @@
 import axios, { AxiosResponse } from "axios";
-import { useQuery, UseQueryResult } from "react-query";
+import { useQueries, useQuery } from "react-query";
+import { UseQueryResult } from "react-query/types/react/types";
 
 import { PokemonResponse } from "../types";
 
-const pokemonApi = (id?: string) =>
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${id || ""}`, {
+export const pokemonApi = (id?: string) => {
+  return axios.get(`https://pokeapi.co/api/v2/pokemon/${id || ""}`, {
     params: { limit: 151 },
   });
-
-const usePokemon = <T>(id?: string): UseQueryResult<T, Error> => {
-  return useQuery(id ? ["pokemon", id] : "pokemon", () => pokemonApi(id));
 };
 
-export default usePokemon;
+export const usePokemonQueries = (
+  names: string[]
+): Array<UseQueryResult<AxiosResponse<PokemonResponse>, Error>> => {
+  const queries = names.map((name, idx) => ({
+    queryKey: ["evolution", `${name}_${idx}`],
+    queryFn: () => pokemonApi(name),
+  }));
+
+  return useQueries(queries) as Array<
+    UseQueryResult<AxiosResponse<PokemonResponse>, Error>
+  >;
+};
+
+const usePokemonQuery = <T>(
+  id?: string
+): UseQueryResult<AxiosResponse<T>, Error> =>
+  useQuery(id ? ["pokemon", id] : "pokemon", () => pokemonApi(id));
+
+export default usePokemonQuery;
